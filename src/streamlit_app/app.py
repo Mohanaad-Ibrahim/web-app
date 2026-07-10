d66710690084cab631239ed0cffd10a88a798d23
zbookg11-coder <zbookg11@gmail.com>
2026-07-10 17:25:23 +0300

    v1

---
+++ src/streamlit_app/app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd 
+from ..services.db.DatabaseManager import DatabaseManager
+
+database_manager = DatabaseManager()
+
+
+st.title("sign up page ")
+username = st.text_input("enter your username ")
+password = st.text_input("enter your password ")
+user_box , password_box = st.columns(2)
+user_box.write(username)
+password_box.write(password)
