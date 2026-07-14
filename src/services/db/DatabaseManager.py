b25282bc2b01e4b495626756c5068314d2a50dc3
zbookg11-coder <zbookg11@gmail.com>
2026-07-14 18:30:12 +0300

    update

--- src/services/db/DatabaseManager.py
+++ src/services/db/DatabaseManager.py
@@ -11,6 +11,7 @@ class DatabaseManager:
         self.username =  os.getenv("MYSQL_USERNAME")
         self.password = os.getenv("MYSQL_PASSWORD")
         self.database_name = os.getenv("MYSQL_DATABASE")
+        self.table_name = os.getenv("MYSQL_TABLE")
 
         self.logger = getLogger(__name__)
 
@@ -18,10 +19,26 @@ class DatabaseManager:
             self.conn= pymysql.Connection(
                 host=self.host,
                 user=self.username,
-                password=self.password,
-                database=self.database_name
+                password=self.password
             )
             self.logger.warning("MySQL Is Working")
 
         except Exception as e :
             self.logger.warning(f"MySQL Is Down !!!\nError: {e}")
+
+    def create_db_and_table_if_not_exists(self):
+
+        my_cursor = self.conn.cursor()
+        my_cursor.execute(f"CREATE DATABASE {self.database_name} IF NOT EXISTS")
+        my_cursor.execute(F"USE {self.database_name}")
+        my_cursor.executemany(f"""
+            CREATE TABLE {self.table_name} IF NOT EXISTS(
+            id INR 
+            
+            )
+        
+    """)
+
+
+
+
