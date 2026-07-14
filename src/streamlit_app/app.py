8e7c66fa77898bbd251a7fee2f56a154c0b2d351
zbookg11-coder <zbookg11@gmail.com>
2026-07-14 20:24:15 +0300

    initial commit

--- src/streamlit_app/app.py
+++ src/streamlit_app/app.py
@@ -1,6 +1,10 @@
+import os, sys
+_here = os.path.dirname(os.path.abspath(__file__))  
+_src  = os.path.abspath(os.path.join(_here, ".."));     sys.path.insert(0, _here);          sys.path.insert(0, _src) 
 import streamlit as st 
 import pandas as pd 
-from src.services.db import DatabaseManager
+from services.db import DatabaseManager
+# cd src && python -m streamlit run streamlit_app/app.py
 
 database_manager = DatabaseManager()
 
@@ -8,6 +12,13 @@ database_manager = DatabaseManager()
 st.title("sign up page ")
 username = st.text_input("enter your username ")
 password = st.text_input("enter your password ")
-user_box , password_box = st.columns(2)
+email = st.text_input("enter your email ")
+user_box , password_box, email_box = st.columns(3)
 user_box.write(username)
 password_box.write(password)
+email_box.write(email)
+
+
+if st.button("submit"):
+    database_manager.insert_into_users_table(username=username,password=password,email=email)
+    st.success("data insert ssuccessfully ")
--- src/streamlit_app/pages/dashboard.py
+++ src/streamlit_app/pages/dashboard.py
@@ -1,12 +1,13 @@
 import streamlit as st 
 import pandas as pd 
-from src.services.db import DatabaseManager
+from services.db import DatabaseManager
 
 database_manager = DatabaseManager()
 
 
 st.title("Dashboard")
-num_rows = st.number_input("enter of num of rows")
+num_rows = int( st.number_input("enter of num of rows", step=1) )
+
 
 
 
